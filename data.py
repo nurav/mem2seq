b9cc2969985a8a2fd7a3f95e17366740e76d327a
Anupma Sharan <anupmasharan@ANUPMA.WV.CC.CMU.EDU>
2019-03-15 15:46:53 -0400

    data preprocessing

--- data.py
+++ data.py
@@ -3,6 +3,9 @@ import torch
 
 from collections import defaultdict
 from torch.utils.data import Dataset, DataLoader
+import pdb 
+
+mem_token_size = 3
 
 class TextDataset(Dataset):
     def __init__(self, x):
@@ -38,26 +41,70 @@ PAD = w2i["<pad>"]
 UNK = w2i["<unk>"]
 
 
-
+def find_entities(filename):
+    bytez = open(filename, 'rb').read()
+    bytez = str(bytez, 'utf-8')
+    kb_entries = set()
+    for line in bytez.splitlines():
+        if '\t' not in line and len(line)>0:
+            kb_entries.add(line.split(' ')[3])
+    return(list(kb_entries))
 # Filters (out channels, in_channels)
 
-def read_dataset(string):
+def read_dataset(string, kb_entries):
     bytez = open(string, 'rb').read()
     bytez = str(bytez, 'utf-8')
     memory = []
     context = []
+    time = 1
 
     for line in bytez.splitlines():
         if len(line) == 0:
             context = []
-        elif '\t' not in line:
-            for word in line.split(' '):
-               memory += w2i[word]
-            # put index of sum of word embeddings here
-        else:
+            time = 1
+            sentinel = []
+            idx = []
 
-        tag, words = line.lower().strip().split("\t")
-        yield ([w2i[x] for x in words.split(" ")], t2i[tag])
+            # dialog_idx = 0
+
+        elif '\t' not in line:
+            context.extend([[word] for  word in line.split(' ')[1:]])
+            
+            # for word in line.split(' '):
+            #     context += word
+               #w2i[word]
+        else:
+            sentinel = [] # gate
+            idx = [] #index
+
+            user, bot = line.split('\t')
+            user = user.split(' ')[1:] # list of words in user utterance
+            
+            context.extend([[word,'$u','t'+str(time)] for word in user])
+            
+            for word in bot.split(' '):
+                index = [i for i,w in enumerate(context) if w[0] == word]
+                # print(index)
+                idx.append(max(index) if index else len(context))
+                sentinel.append(bool(index))
+
+            context_new = context.copy()
+
+            context_new.extend([['$$$$']])
+
+            
+
+            memory.append([context_new, bot, idx, sentinel]) ##### final output
+
+            context.extend([[word,'$s','t'+str(time)] for word in bot.split(' ')])
+
+            time += 1
+
+        # tag, words = line.lower().strip().split("\t")
+        # yield ([w2i[x] for x in words.split(" ")], t2i[tag])
+    # print(memory)
+    return memory
+
 
 
 def read_test(string):
@@ -69,10 +116,12 @@ def read_test(string):
 
 
 # Read in the data
-train = list(read_dataset("topicclass_train.txt"))
+kb_entries = find_entities("data/dialog-bAbi-tasks/dialog-babi-kb-all.txt")
+train = list(read_dataset("data/dialog-bAbi-tasks/dialog-babi-task5trn.txt", kb_entries))
+pdb.set_trace()
 w2i = defaultdict(lambda: UNK, w2i)
-dev = list(read_dataset("topicclass_valid.txt"))
-test = list(read_test("topicclass_test.txt"))
+# dev = list(read_dataset("topicclass_valid.txt"))
+# test = list(read_test("topicclass_test.txt"))
 nwords = len(w2i)
 ntags = len(t2i)
 
@@ -93,4 +142,3 @@ def initialize_dict(path):
         words = line.strip().split(" ")[1:]
         for word in words:
             w2i
-
