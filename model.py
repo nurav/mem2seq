17cd9c0db4ac71d6feec78cd61dee9a98b43ad4f
Anupma Sharan <anupmasharan@ANUPMA.WV.CC.CMU.EDU>
2019-03-16 19:13:53 -0400

    encoder

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import torch
 
 from torch import nn
+import pdb
 
 
 class Model(nn.Module):
@@ -13,11 +14,52 @@ class Model(nn.Module):
 
 
 class Encoder(nn.Module):
-    def __init__(self):
-        pass
+    def __init__(self, hops, nwords, emb_size):
+        super(Encoder, self).__init__()
+        def init_weights(m):
+            if type(m) == torch.nn.Embedding:
+                m.weight.data.fill_(1.0) # intiialize properly!!!
 
-    def forward(self):
-        pass
+        self.hops = hops
+        self.nwords = nwords
+        self.emb_size = emb_size
+
+        #(TODO) : Initialize with word2vec 
+        self.A = torch.nn.ModuleList([torch.nn.Embedding(self.nwords, self.emb_size) for h in range(self.hops)])
+        self.A.apply(init_weights)
+        self.C = torch.nn.ModuleList([torch.nn.Embedding(self.nwords, self.emb_size) for h in range(self.hops)])
+        self.C.apply(init_weights)
+        for i in range(self.hops-1):
+            self.C[i].weight = self.A[i+1].weight
+        self.soft = torch.nn.Softmax(dim = 2)
+        
+
+    def forward(self, context):
+        # context : batchsize x length x 3
+        pdb.set_trace()
+        size = context.size()
+
+        q = torch.zeros(size[0], size[1], self.emb_size) # initialize u # batchsize x length x emb_size
+        q_list = [q] 
+
+        
+
+        context = context.view(size[0],-1) # batchsize x length*3
+        # context = torch.sum(context,2) # batchsize x length
+        for h in range(self.hops):
+            A = self.A[h](context) # batchsize x length*3 x emb_size
+            A = A.view(size[0],size[1],size[2],emb_size) # batchsize x length x 3 x emb_size
+            A = torch.sum(A,2) # batchsize x length x emb_size
+            attn = self.soft(A*q) # batchsize x length x emb_size
+
+            C = self.C[h](context) # batchsize x length*3 x emb_size
+            C = C.view(size[0],size[1],size[2],emb_size) # batchsize x length x 3 x emb_size
+            C = torch.sum(C,2) # batchsize x length x emb_size
+            o = C*attn # batchsize x length x emb_size
+            q += o
+            q_list.append(q)
+        return q
+        
 
 
 class Decoder(nn.Module):
