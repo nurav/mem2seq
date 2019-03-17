63967eeaa636828c667e05e88ab720e1de441d51
misrasaurabh1 <misra.saurabh1@gmail.com>
2019-03-16 21:20:05 -0400

    initial version of decoder

--- model.py
+++ model.py
@@ -40,10 +40,6 @@ class Encoder(nn.Module):
         size = context.size()
 
         q = torch.zeros(size[0], size[1], self.emb_size) # initialize u # batchsize x length x emb_size
-        q_list = [q] 
-
-        
-
         context = context.view(size[0],-1) # batchsize x length*3
         # context = torch.sum(context,2) # batchsize x length
         for h in range(self.hops):
@@ -57,14 +53,54 @@ class Encoder(nn.Module):
             C = torch.sum(C,2) # batchsize x length x emb_size
             o = C*attn # batchsize x length x emb_size
             q += o
-            q_list.append(q)
-        return q
+        return o
         
 
 
 class Decoder(nn.Module):
-    def __init__(self):
-        pass
+    def __init__(self, emb_size, hops, gru_size, nwords, maxlen, batch_size):
+        super(Decoder, self).__init__()
+        self.emb_size = emb_size
+        self.gru_size = gru_size
+        self.hops = hops
+        self.nwords = nwords
+        self.gru = torch.nn.GRU(input_size=self.emb_size,
+                                hidden_size=self.gru_size,
+                                num_layers=1)
+        self.A = torch.nn.ModuleList([torch.nn.Embedding(self.nwords, self.emb_size)\
+                                      for h in range(self.hops)])
+        self.B = torch.nn.ModuleList([torch.nn.Embedding(self.nwords, self.emb_size)\
+                                      for h in range(self.hops)])
 
-    def forward(self):
-        pass
+        for i in range(self.hops-1):
+            self.C[i].weight = self.A[i+1].weight
+        self.soft = torch.nn.Softmax(dim = 2)
+        self.lin_vocab = torch.nn.Linear(2*self.emb_size, self.nwords)
+
+    def forward(self, context, h_, y_):
+
+        _, h = self.gru(y_, h_) #(seq_len, batch, input_size)
+        size = context.size()
+        context = context.view(size[0], -1)
+
+        q = h.copy()
+        o1 = None
+        for hop in range(self.hops):
+            A = self.A[hop](context)
+            A = A.view(size[0], size[1], size[2], self.emb_size)
+            A = torch.sum(A, 2)
+            attn = self.soft(A*q)
+
+            C = self.C[hop](context)  # batchsize x length*3 x emb_size
+            C = C.view(size[0], size[1], size[2], self.emb_size)  # batchsize x length x 3 x emb_size
+            C = torch.sum(C, 2)  # batchsize x length x emb_size
+            o = C * attn  # batchsize x length x emb_size
+            q += o
+            if hop == 0:
+                o1 = o.copy()
+
+        p_vocab = self.soft(self.lin_vocab(torch.cat((h, o1),1)))
+
+        p_ptr = attn
+
+        return h, p_vocab, p_ptr
