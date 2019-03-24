44c4bb0b0315af8285312a36cf6c695c54bf6409
anupma-s <Annie28031996!>
2019-03-24 16:50:06 -0400

    our data, our encoder, our decoder (almost)

--- model_test.py
+++ model_test.py
@@ -2,7 +2,7 @@ import torch
 import numpy as np
 
 from torch import nn
-from masked_cross_entropy import*
+from masked_cross_entropy import *
 
 import numpy as np
 import logging
@@ -16,7 +16,6 @@ from torch import optim
 import torch.nn.functional as F
 from masked_cross_entropy import *
 
-
 TYPE = torch.LongTensor
 TYPEF = torch.FloatTensor
 use_cuda = torch.cuda.is_available()
@@ -25,6 +24,7 @@ if use_cuda:
     TYPE = torch.cuda.LongTensor
     TYPEF = torch.cuda.FloatTensor
     # model.cuda()
+
 
 class Model(nn.Module):
     # def __init__(self, gru_size, nwords, lr, hops, dropout, unk_mask):
@@ -35,11 +35,10 @@ class Model(nn.Module):
         self.nwords = nwords
         self.gru_size = gru_size
         self.emb_size = emb_size
-        assert(self.gru_size == self.emb_size)
-        
+        assert (self.gru_size == self.emb_size)
+
         self.hops = hops
         self.w2i = w2i
-        
 
         self.encoder = Encoder(hops, nwords, gru_size)
         self.decoder = Decoder(emb_size, hops, gru_size, nwords)
@@ -47,7 +46,7 @@ class Model(nn.Module):
         self.optim_enc = torch.optim.Adam(self.encoder.parameters(), lr=0.001)
         self.optim_dec = torch.optim.Adam(self.decoder.parameters(), lr=0.001)
 
-        self.cross_entropy = torch.nn.CrossEntropyLoss() #masked_cross_entropy
+        self.cross_entropy = torch.nn.CrossEntropyLoss()  # masked_cross_entropy
 
         self.loss = 0
         self.ploss = 0
@@ -64,13 +63,12 @@ class Model(nn.Module):
 
     def train_batch(self, context, responses, index, sentinel, new_epoch, context_lengths, target_lengths, clip_grads):
 
-        #(TODO): remove transpose
-        if new_epoch: # (TODO): Change this part
+        # (TODO): remove transpose
+        if new_epoch:  # (TODO): Change this part
             self.loss = 0
             self.ploss = 0
             self.vloss = 0
             self.n = 1
-
 
         context = context.type(TYPE)
         responses = responses.type(TYPE)
@@ -80,15 +78,15 @@ class Model(nn.Module):
         self.optim_enc.zero_grad()
         self.optim_dec.zero_grad()
 
-        h = self.encoder(context.transpose(0,1))
+        h = self.encoder(context.transpose(0, 1))
         self.decoder.load_memory(context.transpose(0, 1))
-        y = torch.from_numpy(np.array([2]*context.size(1), dtype=int)).type(TYPE)
+        y = torch.from_numpy(np.array([2] * context.size(1), dtype=int)).type(TYPE)
         y_len = 0
 
         h = h.unsqueeze(0)
         output_vocab = torch.zeros(max(target_lengths), context.size(1), self.nwords)
         output_ptr = torch.zeros(max(target_lengths), context.size(1), context.size(0))
-        while y_len < responses.size(0): # TODO: Add EOS condition
+        while y_len < responses.size(0):  # TODO: Add EOS condition
             p_ptr, p_vocab, h = self.decoder(context, y, h)
             output_vocab[y_len] = p_vocab
             output_ptr[y_len] = p_ptr
@@ -101,8 +99,8 @@ class Model(nn.Module):
         mask_p = torch.ones(output_ptr.size())
 
         for i in range(responses.size(1)):
-            mask_v[target_lengths[i]:,i,:] = 0
-            mask_p[target_lengths[i]:,i,:] = 0
+            mask_v[target_lengths[i]:, i, :] = 0
+            mask_p[target_lengths[i]:, i, :] = 0
 
         # output_vocab = output_vocab*mask_v
         # output_ptr = output_ptr*mask_p
@@ -125,11 +123,10 @@ class Model(nn.Module):
         torch.save(self.encoder.state_dict(), os.path.join(path, 'encoder.pth'))
         torch.save(self.decoder.state_dict(), os.path.join(path, 'decoder.pth'))
 
-    def load_models(self, path='.'):
+    def load_models(self, path: object = '.') -> object:
         import os
         self.encoder.load_state_dict(os.path.join(path, 'encoder.pth'))
         self.decoder.load_state_dict(os.path.join(path, 'decoder.pth'))
-
 
 
 class Encoder(nn.Module):
@@ -138,7 +135,7 @@ class Encoder(nn.Module):
 
         def init_weights(m):
             if type(m) == torch.nn.Embedding:
-                m.weight.data=torch.normal(0.0,torch.ones(self.nwords,self.emb_size)*0.1)
+                m.weight.data = torch.normal(0.0, torch.ones(self.nwords, self.emb_size) * 0.1)
                 # m.weight.data.fill_(1.0)
 
         self.hops = hops
@@ -146,36 +143,37 @@ class Encoder(nn.Module):
         self.emb_size = emb_size
         self.dropout = 0.2
 
-        #(TODO) : Initialize with word2vec
-        self.A = torch.nn.ModuleList([torch.nn.Embedding(self.nwords, self.emb_size, padding_idx=0) for h in range(self.hops)])
+        # (TODO) : Initialize with word2vec
+        self.A = torch.nn.ModuleList(
+            [torch.nn.Embedding(self.nwords, self.emb_size, padding_idx=0) for h in range(self.hops)])
         self.A.apply(init_weights)
-        self.C = torch.nn.ModuleList([torch.nn.Embedding(self.nwords, self.emb_size, padding_idx=0) for h in range(self.hops)])
+        self.C = torch.nn.ModuleList(
+            [torch.nn.Embedding(self.nwords, self.emb_size, padding_idx=0) for h in range(self.hops)])
         self.C.apply(init_weights)
-        for i in range(self.hops-1):
-            self.C[i].weight = self.A[i+1].weight
+        for i in range(self.hops - 1):
+            self.C[i].weight = self.A[i + 1].weight
         self.soft = torch.nn.Softmax(dim=1)
 
     def forward(self, context):
         # (TODO): Use pack_padded_sequence
         # context : batchsize x length x 3
         # pdb.set_trace()
-        size = context.size() # b x l x 3
+        size = context.size()  # b x l x 3
 
-
-        q = torch.zeros(size[0], self.emb_size).type(TYPEF) # initialize u # batchsize x length x emb_size
+        q = torch.zeros(size[0], self.emb_size).type(TYPEF)  # initialize u # batchsize x length x emb_size
         q_list = [q]
 
-        context = context.view(size[0],-1) # b x l*3
+        context = context.view(size[0], -1)  # b x l*3
         for h in range(self.hops):
-            m = self.A[h](context) # b x l*3 x e
-            m = m.view(size[0],size[1],size[2],self.emb_size) # b x l x 3 x e
-            m = torch.sum(m,2) # b x l x e
-            p = torch.sum(m*q.unsqueeze(1), 2) # b x l (TODO): expand_as(m)
+            m = self.A[h](context)  # b x l*3 x e
+            m = m.view(size[0], size[1], size[2], self.emb_size)  # b x l x 3 x e
+            m = torch.sum(m, 2)  # b x l x e
+            p = torch.sum(m * q.unsqueeze(1), 2)  # b x l (TODO): expand_as(m)
             attn = self.soft(p)
 
-            c = self.C[h](context) # b x l*3 x e
-            c = c.view(size[0],size[1],size[2],self.emb_size) # b x l x 3 x e
-            c = torch.sum(c,2).squeeze(2) # b x l x e
+            c = self.C[h](context)  # b x l*3 x e
+            c = c.view(size[0], size[1], size[2], self.emb_size)  # b x l x 3 x e
+            c = torch.sum(c, 2).squeeze(2)  # b x l x e
             o = torch.bmm(attn.unsqueeze(1), c).squeeze(1)
             q = q + o
         return q
@@ -193,7 +191,7 @@ class Decoder(nn.Module):
 
         def init_weights(m):
             if type(m) == torch.nn.Embedding:
-                m.weight.data=torch.normal(0.0,torch.ones(self.nwords,self.emb_size)*0.1)
+                m.weight.data = torch.normal(0.0, torch.ones(self.nwords, self.emb_size) * 0.1)
 
         self.A = torch.nn.ModuleList(
             [torch.nn.Embedding(self.nwords, self.emb_size, padding_idx=0) for h in range(self.hops)])
@@ -205,16 +203,16 @@ class Decoder(nn.Module):
             self.C[i].weight = self.A[i + 1].weight
 
         self.soft = nn.Softmax(dim=1)
-        self.lin_vocab = nn.Linear(2*emb_size,self.nwords)
+        self.lin_vocab = nn.Linear(2 * emb_size, self.nwords)
         self.gru = nn.GRU(emb_size, emb_size)
 
     def load_memory(self, context):
-        size = context.size() # b * m * 3
+        size = context.size()  # b * m * 3
         self.memories = []
-        context = context.view(size[0],-1)
+        context = context.view(size[0], -1)
         for hop in range(self.hops):
             m = self.A[hop](context)
-            m = m.view(size[0],size[1],size[2],self.emb_size)
+            m = m.view(size[0], size[1], size[2], self.emb_size)
             m = torch.sum(m, 2)
             self.memories.append(m)
             c = self.C[hop](context)
@@ -223,19 +221,17 @@ class Decoder(nn.Module):
         self.memories.append(c)
 
     def forward(self, context, y_, h_):
-        m = self.C[0](y_).unsqueeze(0) # b * e
+        m = self.C[0](y_).unsqueeze(0)  # b * e
         _, h = self.gru(m, h_)
 
         q = [h.squeeze(0)]
         for hop in range(self.hops):
-            p = torch.sum(self.memories[hop]*q[-1].unsqueeze(1).expand_as(self.memories[hop]), 2)
+            p = torch.sum(self.memories[hop] * q[-1].unsqueeze(1).expand_as(self.memories[hop]), 2)
             attn = self.soft(p)
-            o = torch.bmm(attn.unsqueeze(1),self.memories[hop+1]).squeeze(1)
+            o = torch.bmm(attn.unsqueeze(1), self.memories[hop + 1]).squeeze(1)
             q.append(q[-1] + o)
-            if hop==0:
-                p_vocab = self.lin_vocab(torch.cat((q[0], o),1))
+            if hop == 0:
+                p_vocab = self.lin_vocab(torch.cat((q[0], o), 1))
 
         p_ptr = p
         return p_ptr, p_vocab, h
-
-
--- train_test.py
+++ train_test.py
@@ -5,47 +5,52 @@ from tqdm import tqdm
 import argparse
 import datetime
 
-
 from data import find_entities, read_dataset, TextDataset, collate_fn
 
 from model_test import *
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--task", type=str, default='dialog-babi-task1-API-calls')
+parser.add_argument("--personalized", action='store_true', default=False)
 parser.add_argument("--log", action='store_true', default=True)
 parser.add_argument("--lr", type=float, default=0.001)
 parser.add_argument("--val", type=int, default=5)
 parser.add_argument("--name", type=str, default='task5')
 parser.add_argument("-b", type=int, default=8, dest='batch_size')
 parser.add_argument("--cuda", action='store_true', default=True)
+parser.add_argument("--load_from", type=str, default=None)
 
 args = parser.parse_args()
 
 kb_entries = find_entities("data/dialog-bAbI-tasks/dialog-babi-kb-all.txt")
-train, w2i = list(read_dataset(f"data/dialog-bAbI-tasks/{args.task}-trn.txt", kb_entries))
+
+if not args.personalized:
+    train, w2i = list(read_dataset(f"data/dialog-bAbI-tasks/{args.task}-trn.txt", kb_entries))
+
 data = TextDataset(train, w2i)
 batch_size = 8
 train = torch.utils.data.DataLoader(dataset=data,
-                                              batch_size=args.batch_size,
-                                              shuffle=True,
-                                              collate_fn=collate_fn)
+                                    batch_size=args.batch_size,
+                                    shuffle=True,
+                                    collate_fn=collate_fn)
 # pdb.set_trace()
 model = Model(3, len(w2i), 128, 128, w2i)
 
+if args.load_from:
+    model.load_models(args.load_from)
 
 with open(f"log-{str(datetime.datetime.now())}-{args.name}", 'w') as log_file:
     for epoch in range(300):
         pbar = tqdm(enumerate(train), total=len(train))
         for i, batch in pbar:
-            model.train_batch(batch[0].transpose(0,1), batch[1].transpose(0,1), batch[2].transpose(0,1), batch[3].transpose(0,1), i==0, batch[4], batch[5],  8)
+            model.train_batch(batch[0].transpose(0, 1), batch[1].transpose(0, 1), batch[2].transpose(0, 1),
+                              batch[3].transpose(0, 1), i == 0, batch[4], batch[5], 8)
             pbar.set_description(model.print_loss())
             if args.log:
                 print(f"epoch {epoch}: {model.print_loss()}", file=log_file)
 
         if epoch % args.val == 0:
             import os
-            os.makedirs('checkpoints/ckpt-'+str(epoch), exist_ok=True)
-            model.save_models('checkpoints/ckpt-'+epoch)
 
-
-
+            os.makedirs('checkpoints/ckpt-' + str(epoch), exist_ok=True)
+            model.save_models('checkpoints/ckpt-' + str(epoch))
