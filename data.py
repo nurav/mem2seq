8b2825f2070456cd816c30f5c0521a594f761cc8
EC2 Default User <ec2-user@ip-172-31-16-109.us-west-2.compute.internal>
2019-03-25 19:57:59 +0000

    Fixed issues on CUDA

--- data.py
+++ data.py
@@ -82,7 +82,7 @@ def collate_fn(batch):
 
     if use_cuda:
         return torch.from_numpy(out_context).cuda(), torch.from_numpy(out_target).cuda(), torch.from_numpy(out_index).cuda(), \
-           torch.from_numpy(out_gate).cuda(), context_lengths.cuda(), target_lengths.cuda(), out_dialog_idxs.cuda(), context_words.cuda(), target_words.cuda()
+           torch.from_numpy(out_gate).cuda(), context_lengths, target_lengths, torch.from_numpy(out_dialog_idxs).cuda(), context_words, target_words
     else:
         return torch.from_numpy(out_context), torch.from_numpy(out_target), torch.from_numpy(out_index), torch.from_numpy(
             out_gate), context_lengths, target_lengths, out_dialog_idxs, context_words, target_words
