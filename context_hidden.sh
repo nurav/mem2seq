7bdfae1f7a084cbc193266924ce0a5f6c2579b9b
Varun Joshi <vuj@andrew.cmu.edu>
2019-05-02 22:08:24 -0400

    Fixed hidden script

--- context_hidden.sh
+++ context_hidden.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-python3 train.py --model_personalized --data_personalized --log  --task personalized-dialog-task1-API-calls --name task1_hidden --epochs 100 --personalization_type 'hidden'
-python3 plot.py --file plot-data-task1.pkl
-python3 train.py --model_personalized --data_personalized --log  --task personalized-dialog-task2-API-refine --name task2_hidden --epochs 100 --personalization_type 'hidden'
-python3 plot.py --file plot-data-task2.pkl
-python3 train.py --model_personalized --data_personalized --log  --task personalized-dialog-task3-options --name task3_hidden --epochs 100 --personalization_type 'hidden'
-python3 plot.py --file plot-data-task3.pkl
-python3 train.py --model_personalized --data_personalized --log  --task personalized-dialog-task4-info --name task4_hidden --epochs 100 --personalization_type 'hidden'
-python3 plot.py --file plot-data-task4.pkl
-python3 train.py --model_personalized --data_personalized --log  --task personalized-dialog-task5-full-dialogs --name task5_hidden --epochs 100 --personalization_type 'hidden'
-python3 plot.py --file plot-data-task5.pkl
+python3 train.py --task 1 --model hidden --data personal --name task1_hidden --epochs 50 --cuda --log
+python3 plot.py --file plot-data-task1_hidden.pkl
+python3 train.py --task 2 --model hidden --data personal --name task2_hidden --epochs 50 --cuda --log
+python3 plot.py --file plot-data-task2_hidden.pkl
+python3 train.py --task 3 --model hidden --data personal --name task3_hidden --epochs 50 --cuda --log
+python3 plot.py --file plot-data-task3_hidden.pkl
+python3 train.py --task 4 --model hidden --data personal --name task4_hidden --epochs 50 --cuda --log
+python3 plot.py --file plot-data-task4_hidden.pkl
+python3 train.py --task 5 --model hidden --data personal --name task5_hidden --epochs 50 --cuda --log
+python3 plot.py --file plot-data-task5_hidden.pkl
