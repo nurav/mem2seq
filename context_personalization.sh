c6bf38e7b31f875de5379408c8f6f01b35678121
Varun Joshi <vuj@andrew.cmu.edu>
2019-05-02 22:06:05 -0400

    Fixed model name

--- context_personalization.sh
+++ context_personalization.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-python3 train.py --task 1 --model context --data personal --name task1_context --epochs 50 --cuda --log
+python3 train.py --task 1 --model personal_context --data personal --name task1_context --epochs 50 --cuda --log
 python3 plot.py --file plot-data-task1_split.pkl
-python3 train.py --task 2 --model context --data personal --name task2_context --epochs 50 --cuda --log
+python3 train.py --task 2 --model personal_context --data personal --name task2_context --epochs 50 --cuda --log
 python3 plot.py --file plot-data-task2.pkl
-python3 train.py --task 3 --model context --data personal --name task3_context --epochs 50 --cuda --log
+python3 train.py --task 3 --model personal_context --data personal --name task3_context --epochs 50 --cuda --log
 python3 plot.py --file plot-data-task3.pkl
-python3 train.py --task 4 --model context --data personal --name task4_context --epochs 50 --cuda --log
+python3 train.py --task 4 --model personal_context --data personal --name task4_context --epochs 50 --cuda --log
 python3 plot.py --file plot-data-task4.pkl
-python3 train.py --task 5 --model context --data personal --name task5_context --epochs 50 --cuda --log
+python3 train.py --task 5 --model personal_context --data personal --name task5_context --epochs 50 --cuda --log
 python3 plot.py --file plot-data-task5.pkl
