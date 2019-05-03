e2b4f08b9e421789a3e2bb4d251bb54431ebe84c
Varun Joshi <vuj@andrew.cmu.edu>
2019-05-02 22:03:37 -0400

    Changed splitmemory script

--- split_memory.sh
+++ split_memory.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-python3 train.py --model_personalized --data_personalized --log  --task personalized-dialog-task1-API-calls --name task1_split --epochs 100
+python3 train.py --task 1 --model split_mem --data personal --name task1_split --epochs 100 --cuda --log
 python3 plot.py --file plot-data-task1_split.pkl
-python3 train.py --model_personalized --data_personalized --log  --task personalized-dialog-task2-API-refine --name task2_split --epochs 100
+python3 train.py --task 2 --model split_mem --data personal --name task2_split --epochs 100 --cuda --log
 python3 plot.py --file plot-data-task2.pkl
-python3 train.py --model_personalized --data_personalized --log  --task personalized-dialog-task3-options --name task3_split --epochs 100
+python3 train.py --task 3 --model split_mem --data personal --name task3_split --epochs 100 --cuda --log
 python3 plot.py --file plot-data-task3.pkl
-python3 train.py --model_personalized --data_personalized --log  --task personalized-dialog-task4-info --name task4_split --epochs 100
+python3 train.py --task 4 --model split_mem --data personal --name task4_split --epochs 100 --cuda --log
 python3 plot.py --file plot-data-task4.pkl
-python3 train.py --model_personalized --data_personalized --log  --task personalized-dialog-task5-full-dialogs --name task5_split --epochs 100
+python3 train.py --task 5 --model split_mem --data personal --name task5_split --epochs 100 --cuda --log
 python3 plot.py --file plot-data-task5.pkl
